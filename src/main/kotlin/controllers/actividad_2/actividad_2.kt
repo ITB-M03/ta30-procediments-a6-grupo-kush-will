8f4abbc366c2f01b311bb9baef8723b3df23feda
kush <khushbir.kumar.7e8@itb.cat>
2024-12-09 12:55:17 +0100

    f

--- src/main/kotlin/controllers/actividad_2/actividad_2.kt
+++ src/main/kotlin/controllers/actividad_2/actividad_2.kt
@@ -1,42 +1,12 @@
 package controllers.actividad_2
+
+import controllers.ejercicio_1.trasladarPunto
 import java.util.*
 
-fun main () {
+fun main() {
     val scan = inicioscan()
-    println(menuCinema(scan))
-    val salaBuida = buidarSala()
-    val asientosDisponibles = seientsDisponibles()
-    val asientosReservados = reserva()
-    finalscan(scan)
-}
-fun inicioscan ():Scanner {
-    return Scanner(System.`in`).useLocale(Locale.UK)
-}
-
-fun menuCinema(scan: Scanner) {
-    println("Selecciona alguna de estas 3 opciones")
-    println()
-    println("1 - Buidar sala")
-    println()
-    println("2 - Visualitzar seients disponibles")
-    println()
-    println("3 - Reserva de Seients")
-    val pedirNumero = scan.nextInt()
-    if (pedirNumero == 1) {
-        println(buidarSala())
-    }
-    if (pedirNumero == 2) {
-        println(seientsDisponibles())
-    }
-    if (pedirNumero == 3) {
-        println(reserva(salaBuida))
-    }
-}
-
-fun buidarSala(filas:Int=20, columnas: Int= 15): Array<Char> {
-    /*aqui definiremos la matriz que seran los asientos que haya en la sala
-    como las filas y las columnas estan determinadas poir el problema no nos hace falta pedirlo*/
-    //creamos la matriz
+    val filas: Int = 20
+    val columnas: Int = 15
     val matrix = Array(filas) {
         CharArray(columnas) { '_' }
     }
@@ -47,40 +17,85 @@ fun buidarSala(filas:Int=20, columnas: Int= 15): Array<Char> {
             println(matrix[i][j])
         }
     }
-    return buidarSala()
+    buidaSala(matrix)
+    val asientosDisponibles = seientsDisponibles(matrix)
+    val asientosReservados = reserva(matrix)
+    println(menuCinema(scan,matrix))
+    finalscan(scan)
 }
 
-fun seientsDisponibles() {
-    println(reserva(salaBuida ))
+fun inicioscan(): Scanner {
+    return Scanner(System.`in`).useLocale(Locale.UK)
 }
 
-
-fun reserva (salaBuida:Array<Char>) {
-    val matrix2: Array<Char> = salaBuida
-        //recorreremos la matriz y donde nos diga las coordenada la cambiaremos por una X
-    println("Dame las coordenadas para reservar tu asiento")
-        val scanner = Scanner(System.`in`)
-    println("Introduce el numero de Fila:")
-        val numF = scanner.nextInt()
-    println("Introduce el numero de Columna:")
-        val numC = scanner.nextInt()
-        val matrix = Array(numF) {
-            CharArray(numC) { '_' }
+fun menuCinema(scan: Scanner, matrix: Array<CharArray>) {
+    println("Selecciona alguna de estas 3 opciones")
+    println("1 - Buidar sala")
+    println("2 - Visualitzar seients disponibles")
+    println("3 - Reserva de Seients")
+    when (scan.nextInt()) {
+        1 -> {
+            buidaSala(matrix)
         }
-        for (i in 0..numF) {
-            for (j in 0..numC) {
-                if (numF >= 20 && numC >= 15) {
-                    println("ERROR")
-                } else if (numF <= 20 && numC <= 15 && numF > 0 && numC > 0) {
-                    if (i == numF && j == numC) {
-                        matrix[i][j] = 'X'
-                    }
+
+        2 -> {
+            println(seientsDisponibles(matrix))
+        }
+
+        3 -> {
+            println(reserva(matrix))
+        }
+    }
+}
+
+fun buidaSala(matrix: Array<CharArray>) {
+    /*aqui definiremos la matriz que seran los asientos que haya en la sala
+como las filas y las columnas estan determinadas poir el problema no nos hace falta pedirlo*/
+    //creamos la matriz
+    for (i in 0 until matrix.size) {
+        for (j in 0 until matrix[i].size) {
+            matrix[i][j] = '_'
+        }
+    }
+}
+
+fun seientsDisponibles(matrix: Array<CharArray>) {
+    for (i in 0 until matrix.size) {
+        for (j in 0 until matrix[i].size) {
+            matrix[i][j] = '_'
+            println(matrix[i][j])
+        }
+    }
+
+}
+
+
+fun reserva(salaBuida: Array<CharArray>): Array<CharArray> {
+    //recorreremos la matriz y donde nos diga las coordenada la cambiaremos por una X
+    println("Dame las coordenadas para reservar tu asiento")
+    val scanner = Scanner(System.`in`)
+    println("Introduce el numero de Fila:")
+    val numF = scanner.nextInt()
+    println("Introduce el numero de Columna:")
+    val numC = scanner.nextInt()
+    val matrix = Array(numF) {
+        CharArray(numC) { '_' }
+    }
+    for (i in 0..numF) {
+        for (j in 0..numC) {
+            if (numF >= 20 && numC >= 15) {
+                println("ERROR")
+            } else if (numF <= 20 && numC <= 15 && numF > 0 && numC > 0) {
+                if (i == numF && j == numC) {
+                    matrix[i][j] = 'X'
                 }
             }
         }
     }
+    return reserva(salaBuida)
+}
 
-fun finalscan (scan:Scanner){
+fun finalscan(scan: Scanner) {
     scan.close()
 }
 
